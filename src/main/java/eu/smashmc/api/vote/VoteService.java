e00dd278aee434060a2298f85c60ec2d2f817ab9
FlorianWip <florianwippert@gmx.de>
2024-10-27 02:55:39 +0200

    :books: Added documentation for VoteService

--- src/main/java/eu/smashmc/api/vote/VoteService.java
+++ src/main/java/eu/smashmc/api/vote/VoteService.java
@@ -10,14 +10,43 @@ import java.util.concurrent.CompletableFuture;
 @SmashComponent(value = {Environment.BUKKIT}, fallbackImpl = VoteServiceFallback.class)
 public interface VoteService {
 
-
+    /**
+     * Get the current vote streak of a player.
+     *
+     * @param uuid the {@link UUID} of the player
+     * @return A {@link CompletableFuture} completing the vote streak of the player
+     */
     CompletableFuture<Integer> getVoteStreak(UUID uuid);
 
+    /**
+     * Get all votes of a player.
+     *
+     * @param uuid the {@link UUID} of the player
+     * @return A {@link CompletableFuture} completing a {@link List} of all {@link Vote} of the player
+     */
     CompletableFuture<List<Vote>> getVotes(UUID uuid);
 
+    /**
+     * Get all unclaimed votes of a player.
+     *
+     * @param uuid the {@link UUID} of the player
+     * @return A {@link CompletableFuture} completing a {@link List} of all unclaimed {@link Vote} of the player
+     */
     CompletableFuture<List<Vote>> getUnclaimedVotes(UUID uuid);
 
+    /**
+     * Get the last vote of a player.
+     *
+     * @param uuid the {@link UUID} of the player
+     * @return A {@link CompletableFuture} completing the last submitted {@link Vote} of the player
+     */
     CompletableFuture<Vote> getLastVote(UUID uuid);
 
+    /**
+     * Submit a vote as claimed.
+     *
+     * @param voteId the id of the vote
+     * @return A {@link CompletableFuture} completing when the vote has been submitted as claimed
+     */
     CompletableFuture<Void> submitVoteAsClaimed(long voteId);
 }
